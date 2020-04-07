3a83e026ddc10604820951e39e29975cfa205268
soldatov201415 <51156440+soldatov201415@users.noreply.github.com>
2020-04-07 17:39:33 +0300

    Update Java Script.js

--- css/JavaScript/Java Script.js
+++ css/JavaScript/Java Script.js
@@ -1,18 +1,15 @@
 var userName = prompt("Привіт! Як тебе звати?");
 var stranger = "Незнайомець, false або null";
-if (userName == false) {
+if (userName == false){
     userName = "";
     userName = null;
     userName = false;
     if(confirm("ви не ввели ім'я! Можливо ви хочете ввести своє ім'я")){
-       var UserNameTwo = prompt("Введіть своє ім'я, або як би ви хотіли, щоб до вас звертались");
-           
-       }
-    }else{
+       var UserNameTwo = prompt("Введіть своє ім'я, або як би ви хотіли, щоб до вас звертались");           
+       alert(UserNameTwo);
+       }else{
         alert("Ми будем називати вас " + stranger);
         alert("Привіт, Незнайомцю");
         userName = " Незнайомець";
     }
-}else{
-    alert("Привіт, " + userName + "!");
 }
