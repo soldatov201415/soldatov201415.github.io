481c04c46366d75193e7e57bf3adfd5d1714a1d0
soldatov201415 <51156440+soldatov201415@users.noreply.github.com>
2020-05-11 18:27:30 +0300

    Update Java Script.js

--- css/JavaScript/Java Script.js
+++ css/JavaScript/Java Script.js
@@ -6,8 +6,9 @@ if (userName == false){
     userName = null;
     userName = false;
     if(confirm("ви не ввели ім'я! Можливо ви хочете ввести своє ім'я")){
-       var UserNameTwo = prompt("Введіть своє ім'я, або як би ви хотіли, щоб до вас звертались");           
-       alert(UserNameTwo);
+       var UserNameTwo = prompt("Введіть своє ім'я, або як би ви хотіли, щоб до вас звертались");
+       alert("Привіт" + UserNameTwo);
+       UserNameTwo = UserName;           
        if (UserNameTwo == false) {
             UserNameTwo = 0;
             UserNameTwo ="";
@@ -15,10 +16,14 @@ if (userName == false){
             alert("Ми будем називати вас " + stranger);
             userName = strangers;
         }
+        
        }else{
         alert("Ми будем називати вас " + stranger);
         alert("Привіт, Незнайомцю");
         userName = " Незнайомець";
     }
 }
-
+    
+if(UserNameTwo ==! false){
+            alert("Привіт," + UserNameTwo + "!");
+        }
